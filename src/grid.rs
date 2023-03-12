b2d559528892c2d24abee8cdcf97ac4583e1e25e
Guilhem Santé <guilhem.snt@gmail.com>
2023-03-12 14:38:36 +0100

    [UPDATE] : try Grid type implementation with testing Grid struct

--- src/grid.rs
+++ src/grid.rs
@@ -1,16 +1,41 @@
-enum Status {
-    Nothing,
-    Mine,
-    NearBomb(u8),
+// enum Status {
+//     Nothing,
+//     Mine,
+//     NearBomb(u8),
+// }
+
+// struct Cell {
+//     status: Status,
+//     discovered: bool,
+// }
+
+// type Grid = Vec<Vec<Cell>>;
+
+use std::fmt;
+
+pub struct Grid {
+    rows: Vec<Vec<char>>,
 }
 
-struct Cell {
-    status: Status,
-    discovered: bool,
+impl From<String> for Grid {
+    fn from(s: String) -> Self {
+        let mut rows = Vec::new();
+        for line in s.lines() {
+            let row = line.chars().collect();
+            rows.push(row);
+        }
+        Grid { rows }
+    }
 }
 
-type Grid = Vec<Vec<Cell>>;
-
-pub fn print() {
-    println!("Salut, Monde!");
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for row in &self.rows {
+            for c in row {
+                write!(f, "{}", c)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod grid;
 
 fn main() {
-    grid::print();
+    let grid_str = String::from("ABC\nDEF\nGHI");
+
+    let grid = grid::Grid::from(grid_str);
+
+    println!("grid:\n{}", grid);
 }
